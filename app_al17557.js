420e11e88a34d831dc4011bfd9b91a79933b2313
Osuna Tirado Juan Roman <al17557@cbtis051.edu.mx>
2019-11-21 16:35:22 -0700

    Commit inicial del server

---
+++ app_al17557.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function (res, res) {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+
+    res.setEncoding('Hola Mundo Node JS Repo Git - Osuna Tirado Juan Roman\n');
+}).listen(8081);
+console.log('Server running on port 8081');
