5e48e0c981bbf4b30cc9b5b14bd1b9d35bcd97b8
Osuna Tirado Juan Roman <al17557@cbtis051.edu.mx>
2019-11-26 15:22:16 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17557.js
+++ app_al17557.js
@@ -2,6 +2,12 @@ var http = require('http');
 http.createServer(function (res, res) {
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola Mundo Node JS Repo Git - Osuna Tirado Juan Roman\n');
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Osuna Tirado Juan Roman\n'
+    };
+
+    var json = JSON.stringify(obejto);
+
+    res.end(json);
 }).listen(8081);
 console.log('Server running on port 8081');
