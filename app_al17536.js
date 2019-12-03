6f4b2c178f7c5dabf6827d1321056aae101741f6
Bernal Zatarain Oscar Ramiro <al17536@cbtis.edu.mx>
2019-12-03 14:20:48 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17536.js
+++ app_al17536.js
@@ -2,6 +2,12 @@ var http = require('http');
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GIT - Bernal Zatarain Oscar Ramiro\n');
+    var objeto ={
+        mensaje : 'Hola mundo Node JS Repo GIT - Bernal Zatarain Oscar Ramiro\n'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
 }).listen(8081);
 console.log('Server running on port 8081');
