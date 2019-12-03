6b96f5342c8c51dfeab0e3900a461835564463e1
Angulo Rodriguez Eduardo <al17483@cbtis051.edu.mx>
2019-12-03 14:28:28 -0700

    se agrega app_al17483.js

--- app_al17483.js
+++ app_al17483.js
@@ -1,10 +1,15 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    res.writeHead(200, {'content-type': 'application/json; charset=utf-8'});
 
-    var objeto ={
-        mensaje : 'Hola mundo Node JS Repo GIT - Angulo Rodriguez Eduardo\n'}
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GIT - Angulo Rodriguez Eduardo\n');
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Angulo Rodriguez Eduardo Hazael\n'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+    
 }).listen(8081);
 console.log('Server running on port 8081');
