bff4253bbcfa0e4f2b19f98ec74ec9c1565e072a
Angulo Rodriguez Eduardo <al17483@cbtis051.edu.mx>
2019-12-03 14:16:27 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17483.js
+++ app_al17483.js
@@ -2,6 +2,9 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'content-type': 'application/json; charset=utf-8'});
 
+    var objeto ={
+        mensaje : 'Hola mundo Node JS Repo GIT - Angulo Rodriguez Eduardo\n'}
+
     res.end('Hola mundo Node JS Repo GIT - Angulo Rodriguez Eduardo\n');
 }).listen(8081);
 console.log('Server running on port 8081');
